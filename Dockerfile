d6df962b7d86ac8884c2baa1875b2f20e0687377
carneirofc <claudiofcarneiro@hotmail.com>
2022-04-10 18:45:03 -0300

    Include gh ci

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-FROM busybox:1.35 as builder
-ARG PRE_COMMIT_HOOKS_VERSION=v4.2.0
-ADD https://github.com/pre-commit/pre-commit-hooks/archive/refs/tags/${PRE_COMMIT_HOOKS_VERSION}.tar.gz /pre-commit-hooks.tar.gz
-RUN tar -zxvf /pre-commit-hooks.tar.gz && rm /pre-commit-hooks.tar.gz && mv /pre-commit-hooks* /pre-commit-hooks
-
 FROM python:3.9.12-alpine3.15
-RUN pip install pre-commit==2.18.1 && mkdir /pre-commit
+ENV PRE_COMMIT_HOOKS_VERSION v4.2.0
+RUN pip install pre-commit==2.18.1 && \
+    mkdir /pre-commit && \
+    apk add git && \
+    git clone https://github.com/pre-commit/pre-commit-hooks && \
+    cd pre-commit-hooks && git checkout ${PRE_COMMIT_HOOKS_VERSION}
 WORKDIR /pre-commit
 
-COPY --from=builder /pre-commit-hooks /pre-commit-hooks
 COPY ./entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/bin/sh", "/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,21 @@
 if [ ! -f /pre-commit/.pre-commit-config.yaml ]; then
 cat << EOF > /pre-commit/.pre-commit-config.yaml
 repos:
--   repo: https://github.com/pre-commit/pre-commit-hooks
-    rev: /pre-commit-hooks
-    hooks:
-    -   id: trailing-whitespace
-    -   id: end-of-file-fixer
-    -   id: check-yaml
-    -   id: check-json
-    -   id: debug-statements
-    -   id: double-quote-string-fixer
-    -   id: name-tests-test
-    -   id: requirements-txt-fixer
-    -   id: mixed-line-ending
+  - repo: https://github.com/pre-commit/pre-commit-hooks
+  rev: ${PRE_COMMIT_HOOKS_VERSION}
+  hooks:
+    - id: trailing-whitespace
+    - id: end-of-file-fixer
+    - id: check-yaml
+    - id: check-json
+    - id: debug-statements
+    - id: double-quote-string-fixer
+    - id: name-tests-test
+    - id: requirements-txt-fixer
+    - id: mixed-line-ending
 EOF
+    pre-commit run --all-files .
+    rm /pre-commit/.pre-commit-config.yaml
+else
+    pre-commit run --all-files .
 fi
-
-if [ ! -d .git/ ]; then
-echo "Git repo not found"
-fi
-
-pre-commit run --all-files .
